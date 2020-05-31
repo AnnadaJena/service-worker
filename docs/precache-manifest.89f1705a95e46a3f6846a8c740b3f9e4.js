25b9898d9a353dcf7407c5a7ec51c5765f14ebaf
Annada Jena <annada.jena@rakuten.com>
2020-05-31 20:31:10 +0530

    first build added

---
+++ docs/precache-manifest.89f1705a95e46a3f6846a8c740b3f9e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2015f770b6cfeafe1c215f5b32b430af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2babc329fb8402ad809",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "35808bb46229ee0d9a70",
+    "url": "/static/js/2.91f5d127.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.91f5d127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2babc329fb8402ad809",
+    "url": "/static/js/main.7a6a4eca.chunk.js"
+  },
+  {
+    "revision": "1ded7db9b589f294c1af",
+    "url": "/static/js/runtime-main.c8b39257.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
